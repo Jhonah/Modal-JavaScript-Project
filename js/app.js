31d97d730a51582f8d83db668cc71bfc4f737d16
Jhonah <buryakone@gmail.com>
2020-10-13 01:29:18 +0700

    сделал чтобы при к клике на картинку, она увеливалась и закрывалась

--- js/app.js
+++ js/app.js
@@ -28,13 +28,38 @@ const searchBox = document.querySelector("#search-item");
 
 searchBox.addEventListener('keyup', (inputEvent) => {
 
-    const searchFilter = inputEvent.target.value.toLowerCase();
+    const searchFilter = inputEvent.target.value.toLowerCase().trim();
 
     storeItems.forEach((item) => {
         if (item.textContent.includes(searchFilter)) {
             item.style.display = 'block';
         } else {
             item.style.display = 'none';
+        }
+    })
+})
+
+const boxContainer = document.querySelector('.lightbox-container');
+const boxButtons = document.querySelectorAll('.img-container');
+
+boxButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        boxContainer.classList.add('show');
+    })
+})
+
+const btnClose = document.querySelector('.lightbox-close');
+
+btnClose.addEventListener('click', () => {
+    boxContainer.classList.remove('show');
+})
+
+const boxControls = document.querySelectorAll('.lightbox-control');
+
+boxControls.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.includes('btnLeft')) {
+            console.log('я жму кнопку влево');
         }
     })
 })
